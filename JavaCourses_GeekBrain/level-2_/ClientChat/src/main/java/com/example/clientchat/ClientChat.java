6d4f54d7dfe92cecd933772dfe4933e64066c11f
Azamat-Kurmanov <azik.kurmanov@gmail.com>
2022-04-23 00:29:12 +0600

    Практическое задание №4. Java уровень-2

--- JavaCourses_GeekBrain/level-2_/ClientChat/src/main/java/com/example/clientchat/ClientChat.java
+++ JavaCourses_GeekBrain/level-2_/ClientChat/src/main/java/com/example/clientchat/ClientChat.java
@@ -7,13 +7,16 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ClientChat extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientChat.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Java FX Application");
         stage.setScene(scene);
+
+        ClientController controller = fxmlLoader.getController();
+        controller.userList.getItems().addAll("user1","user2");
         stage.show();
     }
 
